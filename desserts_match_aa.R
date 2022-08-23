3e0da3fc935cf2c26f61b5ef658a498349237c17
hveirs <hveirs3@outlook.com>
2022-08-23 21:59:58 +0000

    Added new code

---
+++ desserts_match_aa.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(here)
+library(rvest)
+
+
+iconic_list <- read_csv(here("iconic_desserts.csv"))
+fav_desserts <- read_csv(here("favorite_desserts.csv"))
+
+new_fav_desserts <- fav_desserts |> rename(dessert = Favorite_dessert)
+
+
+summary_desserts <- c()
+# We want to iterate along our favorites list to see how many matches on the iconic list there are.
+
+#for(i in seq_along(fav_desserts$Favorite_dessert)) {
+  #fav <- fav_desserts$Favorite_dessert[i]
+  #for(j in seq_along(iconic_list$dessert)) {
+    #if (str_detect(iconic_list$dessert[j], "cake") = TRUE) {
+     #
+    #}
+    #
+    #}
+#}
